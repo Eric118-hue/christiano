1341c4d2b006fa2bfe710f4d0da1401b60b70c3a
Eric118-hue <jefabrice@gmail.com>
2022-09-16 21:23:16 +0200

    webpack

---
+++ env.js
@@ -0,0 +1,21 @@
+import __logo from './medias/images/topmora.svg';
+import Parsley from 'parsleyjs';
+import 'parsleyjs/dist/i18n/fr';
+import 'parsleyjs/dist/i18n/fr.extra';
+
+const laravel = '/var/www/macentrale/gha/'
+
+const config = {
+    laravel : {
+        public : `${laravel}public/`,
+        root : laravel
+    },
+
+}
+
+
+export const languages = {};
+
+export const logo = __logo;
+
+export default config;
